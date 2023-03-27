52519f6a4937617a4284b901781745b7c5d0ec5f
Jonathan Hollingsworth <jonathanhollingsworth07@gmail.com>
2023-03-27 12:43:43 -0400

    Edited docstring

--- words.py
+++ words.py
@@ -1,6 +1,6 @@
 alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 def print_upper_words(words, must_start_with=alphabet):
-    """Prints all given words that match the (must_start_with) condition in full upper case
+    """Prints all given words that match the (must_start_with) condition in full Uppercase
        (if there is no condition given then it will print all words given)
        
        Example:
